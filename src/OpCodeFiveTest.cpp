c1bdcf2b1fe74751532a45590fda472d8ead025a
Georgios Charitos <geoxarg@gmail.com>
2020-11-21 21:32:36 +0000

    Increased OpCodeFive code test coverage.

--- src/OpCodeFiveTest.cpp
+++ src/OpCodeFiveTest.cpp
@@ -15,11 +15,37 @@ TEST_CASE("OpCodeFive - Single element, input shall not be processed", "[OpCodeP
     CHECK(modifiedInput[0] == 5);
 }
 
-TEST_CASE("OpCodeFive - Move the instruction pointer to the end of the input and then exit", "[OpCodeProcessor]")
+TEST_CASE("OpCodeFive - First parameter is zero, do nothing (ParameterMode::Immediate)", "[OpCodeProcessor]")
+{
+    std::vector<int> input =
+    {
+        105,0,99
+    };
+
+    OpCodeProcessor opCodeProcessor;
+    auto [modifiedInput, printedOut] = opCodeProcessor.ProcessInstructions<int>(input);
+    REQUIRE(modifiedInput.size() == input.size());
+    CHECK(std::equal(modifiedInput.begin(), modifiedInput.end(), input.begin()));
+}
+
+TEST_CASE("OpCodeFive - Move the instruction pointer to the end of the input and then exit (ParameterMode::Position)", "[OpCodeProcessor]")
 {
     std::vector<int> input =
     {
         5,11,1,2,3,4,5,6,7,8,9,99
+    };
+
+    OpCodeProcessor opCodeProcessor;
+    auto [modifiedInput, printedOut] = opCodeProcessor.ProcessInstructions<int>(input);
+    REQUIRE(modifiedInput.size() == input.size());
+    CHECK(std::equal(modifiedInput.begin(), modifiedInput.end(), input.begin()));
+}
+
+TEST_CASE("OpCodeFive - Move the instruction pointer to the end of the input and then exit (ParameterMode::Immediate)", "[OpCodeProcessor]")
+{
+    std::vector<int> input =
+    {
+        1105,1,11,2,3,4,5,6,7,8,9,99
     };
 
     OpCodeProcessor opCodeProcessor;
