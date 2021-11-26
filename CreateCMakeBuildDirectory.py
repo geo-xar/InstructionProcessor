224d88fc2b9053a890b7e708f804659fe3874869
Georgios Charitos <geoxarg@gmail.com>
2021-11-26 22:32:01 +0000

    Tidy up CMake files and CreateCMakeBuildDirectory.py

--- CreateCMakeBuildDirectory.py
+++ CreateCMakeBuildDirectory.py
@@ -6,116 +6,72 @@ from sys import platform
 import argparse
 
 class CmdLineArgParser:
-
     def is_valid_configuration(self, configuration):
-
         config = str(configuration).lower()
-
         if config == 'release' or config == 'debug':
-
             return config[0].upper() + config[1:]
-
         else:
-
             raise argparse.ArgumentTypeError(f"Invalid configuration: {configuration}")
 
-
     def parse(self):
-
         self.parser = argparse.ArgumentParser(description='Command Line Argument Parser')
-
         self.parser.add_argument('--configuration', type=self.is_valid_configuration, required=False, default='Debug', help='Build configuration i.e. Debug or Release')
-
-        self.parser.add_argument('--compiler', type=str, required=False, default='GCC', help='Default compiler in Linux is GCC and in Windows is MSVC. Please specify your desired compiler i.e. Clang')
-
+        self.parser.add_argument('--compiler', type=str, required=False, default='g++', help='Default compiler in Linux is GCC and in Windows is MSVC. Please specify your desired compiler i.e. Clang')
         self.args = self.parser.parse_args()
 
-
     def get_config(self):
-
         return self.args.configuration
 
-
     def get_compiler(self):
-
         return self.args.compiler
 
-
 def main():
-
     cmdLineArgParser = CmdLineArgParser()
-
     cmdLineArgParser.parse()
 
     build_directory = "Build" + cmdLineArgParser.get_config()
-
     build_directory_exists = False
 
     if path.exists(build_directory):
+        build_directory_exists = True
 
-        user_input = input("The {0} directory already exists. Do you wish to:\n1.Build the already existing project\n2.Delete it and build from scratch\n3.Do nothing\nPlease choose between 1, 2 and 3\n".format(build_directory))
+        user_input = str(input("The {0} directory already exists. Do you wish to:\n1.Delete it and generate it from scratch\n2.Do nothing\nPlease choose between 1 and 2\n".format(build_directory)))
 
-        while not (user_input == "1" or user_input == "2" or user_input == "3"):
+        while (user_input != '1' and user_input != '2'):
+            user_input = str(input("The {0} directory already exists. Do you wish to:\n1.Delete it and generate it from scratch\n2.Do nothing\nPlease choose between 1 and 2\n".format(build_directory)))
 
-            user_input = input("The {0} directory already exists. Do you wish to:\n1.Build the already existing project\n2.Delete it and build from scratch\n3.Do nothing\nPlease choose between 1, 2 and 3\n".format(build_directory))
-
-        if user_input == "1":
-
-            build_directory_exists = True
-
-        elif user_input == "2":
-
+        if user_input == '1':
             if platform == "linux" or platform == "linux2":
-
                 system('rm -rf {0}'.format(build_directory))
-
             elif platform == "win32":
-
                 system('rmdir /s /q {0}'.format(build_directory))
 
-        else:
+            build_directory_exists = False
 
+        else:
             exit("The {0} directory will remain intact.\n".format(build_directory))
 
     if not build_directory_exists:
-
         system('mkdir {0}'.format(build_directory))
 
     chdir('{0}'.format(build_directory))
 
-    if not build_directory_exists:
+    cmakeCommand = 'cmake ../ -DCMAKE_BUILD_TYPE={0}'.format(cmdLineArgParser.get_config())
 
-        code_coverage = "ON"
+    if cmdLineArgParser.get_config() == 'Debug':
+	    cmakeCommand = cmakeCommand + ' -DCODE_COVERAGE=ON'
 
-        if cmdLineArgParser.get_config() == "Release":
+    if cmdLineArgParser.get_compiler().lower() == 'clang':
+        if platform == 'linux' or platform == 'linux2':
+            cmakeCommand = cmakeCommand + ' -DCMAKE_CXX_COMPILER=clang++'
+        elif platform == 'win32':
+            cmakeCommand = cmakeCommand + ' -T ClangCL'
 
-            code_coverage = "OFF"
+    if platform == 'win32':
+        cmakeCommand = cmakeCommand + ' -A x64'
 
-        if cmdLineArgParser.get_compiler().lower() == 'clang':
-
-            if platform == "linux" or platform == "linux2":
-
-                system('cmake ../ -DCMAKE_BUILD_TYPE={0} -DCODE_COVERAGE={1} -DCMAKE_CXX_COMPILER={2}'.format(cmdLineArgParser.get_config(), code_coverage, 'clang++'))
-
-            elif platform == "win32":
-
-                system('cmake ../ -T ClangCL -A x64')
-
-        else:
-
-            if platform == "linux" or platform == "linux2":
-
-                system('cmake ../ -DCMAKE_BUILD_TYPE={0} -DCODE_COVERAGE={1}'.format(cmdLineArgParser.get_config(), code_coverage))
-
-            elif platform == "win32":
-
-                system('cmake ../ -A x64')
-
-    if platform == "linux" or platform == "linux2":
-
-        system('make -j4')
+    system(cmakeCommand)
 
 
 if __name__ == "__main__":
-
     main()
