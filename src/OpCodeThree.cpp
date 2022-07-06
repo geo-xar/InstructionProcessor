fd84bf67bed808cff8acb1b6b04c9ab0c7a0cfe2
Georgios Charitos <geoxarg@gmail.com>
2022-07-06 21:43:17 +0100

    Moved OpCodeThree implementation to a source file.

---
+++ src/OpCodeThree.cpp
@@ -0,0 +1,37 @@
+// Copyright 2022 by Georgios Charitos.
+// All rights reserved.
+
+#include "OpCodeThree.h"
+
+using namespace InstructionProcessor;
+
+template <typename InputType, typename IteratorType, typename SetElementAtIndexFunctionType>
+OpCodeThree<InputType, IteratorType, SetElementAtIndexFunctionType>::OpCodeThree(
+    SetElementAtIndexFunctionType& setElementAtIndex, const std::optional<InputType>& userInput)
+    : _setElementAtIndex{ setElementAtIndex }, _userInput{ userInput }
+{}
+
+template <typename InputType, typename IteratorType, typename SetElementAtIndexFunctionType>
+OpCode::ReturnType OpCodeThree<InputType, IteratorType, SetElementAtIndexFunctionType>::Execute(std::any& nextElementIter, std::any& endIter)
+{
+    IteratorType& iterBegin = std::any_cast<IteratorType&>(nextElementIter);
+    IteratorType& iterEnd = std::any_cast<IteratorType&>(endIter);
+
+    // Check if there are enough numbers to be claimed to complete the operation.
+    // A single number is needed (index) to store the user input.
+    if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 1))
+    {
+        return { std::nullopt, {} };
+    }
+
+    // Store the user input value.
+    assert(_userInput.has_value());
+    _setElementAtIndex(*iterBegin, _userInput.value());
+
+    // Jump to the next number (if any).
+    iterBegin++;
+
+    // What we return here it is only useful for error reporting.
+    // Whatever different than std::nullopt is equal to SUCCESS.
+    return { std::make_optional<Result>(), {iterBegin} };
+}
--- src/OpCodeThree.h
+++ src/OpCodeThree.h
@@ -19,38 +19,14 @@ public:
     * @param setElementAtIndex Function to store an element to the given index.
     * @param userInput User selection optional.
     */
-    OpCodeThree(SetElementAtIndexFunctionType &setElementAtIndex, const std::optional<InputType> &userInput)
-            : _setElementAtIndex{setElementAtIndex}, _userInput{userInput}
-    {}
+    OpCodeThree(SetElementAtIndexFunctionType &setElementAtIndex, const std::optional<InputType> &userInput);
 
     ~OpCodeThree() final = default;
 
     /**
     * Claim a number and store the user input value to the index which it points to.
     */
-    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final
-    {
-        IteratorType &iterBegin = std::any_cast<IteratorType &>(nextElementIter);
-        IteratorType &iterEnd = std::any_cast<IteratorType &>(endIter);
-
-        // Check if there are enough numbers to be claimed to complete the operation.
-        // A single number is needed (index) to store the user input.
-        if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 1))
-        {
-            return {std::nullopt, {}};
-        }
-
-        // Store the user input value.
-        assert(_userInput.has_value());
-        _setElementAtIndex(*iterBegin, _userInput.value());
-
-        // Jump to the next number (if any).
-        iterBegin++;
-
-        // What we return here it is only useful for error reporting.
-        // Whatever different than std::nullopt is equal to SUCCESS.
-        return {std::make_optional<Result>(), {iterBegin}};
-    }
+    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final;
 
 private:
     SetElementAtIndexFunctionType &_setElementAtIndex;
