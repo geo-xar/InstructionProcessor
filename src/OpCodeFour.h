72dd60c38a82882244a7e6ac4ca80a38ab2019a2
Georgios Charitos <geoxarg@gmail.com>
2022-07-06 21:48:25 +0100

    Moved OpCodeFour implementation to a source file.

--- src/OpCodeFour.h
+++ src/OpCodeFour.h
@@ -23,51 +23,14 @@ public:
     OpCodeFour(
             GetElementAtFunctionType &getElementAt,
             const ParameterModeVector &parameterModes,
-            std::vector<InputType> &printedOutput)
-            : _getElementAt{getElementAt}, _parameterModes{parameterModes}, _printedOutput{printedOutput}
-    {}
+            std::vector<InputType> &printedOutput);
 
     ~OpCodeFour() final = default;
 
     /**
     * Claim a number and store it in the printed output collection.
     */
-    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final
-    {
-        IteratorType &iterBegin = std::any_cast<IteratorType &>(nextElementIter);
-        IteratorType &iterEnd = std::any_cast<IteratorType &>(endIter);
-
-        // Check if there are enough numbers to be claimed to complete the operation.
-        // A single number is needed to be stored in the printed output collection.
-        if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 1))
-        {
-            return {std::nullopt, {}};
-        }
-
-        InputType number;
-        if (_parameterModes[0] == ParameterMode::Immediate)
-        {
-            number = *iterBegin;
-        }
-        else if (_parameterModes[0] == ParameterMode::Position)
-        {
-            number = _getElementAt(iterBegin);
-        }
-        // ParameterMode::Relative
-        else
-        {
-            number = _getElementAt(iterBegin, {});
-        }
-
-        _printedOutput.emplace_back(number);
-
-        // Jump to the next number (if any).
-        iterBegin++;
-
-        // What we return here it is only useful for error reporting.
-        // Whatever different than std::nullopt is equal to SUCCESS.
-        return {std::make_optional<Result>(), {iterBegin}};
-    }
+    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final;
 
 private:
     GetElementAtFunctionType &_getElementAt;
