2a9eae1d341fb07d63cca5fb5eecf5591330528f
Georgios Charitos <geoxarg@gmail.com>
2022-07-06 21:59:39 +0100

    Moved OpCodeSeven implementation to a source file.

--- src/OpCodeSeven.h
+++ src/OpCodeSeven.h
@@ -26,9 +26,7 @@ public:
     OpCodeSeven(
             SetElementAtIndexFunctionType &setElementAtIndex,
             GetElementAtFunctionType &getElementAt,
-            const ParameterModeVector &parameterModes)
-            : _setElementAtIndex{setElementAtIndex}, _getElementAt{getElementAt}, _parameterModes{parameterModes}
-    {}
+            const ParameterModeVector &parameterModes);
 
     ~OpCodeSeven() final = default;
 
@@ -36,54 +34,7 @@ public:
     * If the first parameter is less than the second parameter then
     * store 1 in the position given by the third parameter, otherwise store 0.
     */
-    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final
-    {
-        IteratorType &iterBegin = std::any_cast<IteratorType &>(nextElementIter);
-        IteratorType &iterEnd = std::any_cast<IteratorType &>(endIter);
-
-        // Check if there are enough numbers to be claimed to complete the operation.
-        // Numbers to be claimed are 2 for the comparison and 1 for the index to store the result.
-        if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, NumberOfParametersToClaim + 1))
-        {
-            return {std::nullopt, {}};
-        }
-
-        // Claim the numbers to be compared based on the parameter modes.
-        std::vector<InputType> claimedComparisonNumbers;
-        for (IndexType i = 0; i < NumberOfParametersToClaim; i++)
-        {
-            if (_parameterModes[i] == ParameterMode::Immediate)
-            {
-                claimedComparisonNumbers.emplace_back(*iterBegin);
-            }
-            else if (_parameterModes[i] == ParameterMode::Position)
-            {
-                claimedComparisonNumbers.emplace_back(_getElementAt(iterBegin));
-            }
-            // ParameterMode::Relative
-            else
-            {
-                claimedComparisonNumbers.emplace_back(_getElementAt(iterBegin, {}));
-            }
-            iterBegin++;
-        }
-
-        if (claimedComparisonNumbers[0] < claimedComparisonNumbers[1])
-        {
-            _setElementAtIndex(*iterBegin, 1);
-        }
-        else
-        {
-            _setElementAtIndex(*iterBegin, 0);
-        }
-
-        // Jump to the next number (if any).
-        iterBegin++;
-
-        // What we return here it is only useful for error reporting.
-        // Whatever different than std::nullopt is equal to SUCCESS.
-        return {std::make_optional<Result>(), {iterBegin}};
-    }
+    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final;
 
 private:
     SetElementAtIndexFunctionType &_setElementAtIndex;
