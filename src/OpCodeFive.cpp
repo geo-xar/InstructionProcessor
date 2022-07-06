8493e73a233b227dfebb83e9ffd70d5972975bd1
Georgios Charitos <geoxarg@gmail.com>
2022-07-06 21:54:05 +0100

    Moved OpCodeFive implementation to a source file.

---
+++ src/OpCodeFive.cpp
@@ -0,0 +1,75 @@
+// Copyright 2022 by Georgios Charitos.
+// All rights reserved.
+
+#include "OpCodeFive.h"
+
+using namespace InstructionProcessor;
+
+template <typename InputType, typename IteratorType, typename GetElementAtFunctionType, typename GetIterFromPosPlusOffsetFunctionType>
+OpCodeFive<InputType, IteratorType, GetElementAtFunctionType, GetIterFromPosPlusOffsetFunctionType>::OpCodeFive(
+    GetElementAtFunctionType& getElementAt,
+    GetIterFromPosPlusOffsetFunctionType& getIterFromPosPlusOffset,
+    const ParameterModeVector& parameterModes)
+    : _getElementAt{ getElementAt }, _getIterFromPosPlusOffset{ getIterFromPosPlusOffset },
+    _parameterModes{ parameterModes }
+{}
+
+template <typename InputType, typename IteratorType, typename GetElementAtFunctionType, typename GetIterFromPosPlusOffsetFunctionType>
+OpCode::ReturnType OpCodeFive<InputType, IteratorType, GetElementAtFunctionType, GetIterFromPosPlusOffsetFunctionType>::Execute(
+    std::any& nextElementIter, std::any& endIter)
+{
+    IteratorType& iterBegin = std::any_cast<IteratorType&>(nextElementIter);
+    IteratorType& iterEnd = std::any_cast<IteratorType&>(endIter);
+
+    // Check if there are enough numbers to be claimed to complete the operation.
+    if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 2))
+    {
+        return { std::nullopt, {} };
+    }
+
+    // Claim the first parameter based on parameter mode.
+    InputType option;
+    if (_parameterModes[0] == ParameterMode::Immediate)
+    {
+        option = *iterBegin;
+    }
+    else if (_parameterModes[0] == ParameterMode::Position)
+    {
+        option = _getElementAt(iterBegin);
+    }
+    // ParameterMode::Relative
+    else
+    {
+        option = _getElementAt(iterBegin, {});
+    }
+
+    // Jump to the next number no matter what the option value is.
+    iterBegin++;
+
+    // If zero number then do nothing.
+    if (option == 0)
+    {
+        // Skip the number.
+        iterBegin++;
+        return { std::make_optional<Result>(), {iterBegin} };
+    }
+
+    // Move the instruction pointer to the index pointed by the second number.
+    InputType number;
+    if (_parameterModes[1] == ParameterMode::Immediate)
+    {
+        number = *iterBegin;
+    }
+    // ParameterMode::Position
+    else
+    {
+        number = _getElementAt(iterBegin);
+    }
+
+    // Update instruction pointer.
+    iterBegin = _getIterFromPosPlusOffset(number);
+
+    // What we return here it is only useful for error reporting.
+    // Whatever different than std::nullopt is equal to SUCCESS.
+    return { std::make_optional<Result>(), {iterBegin} };
+}
--- src/OpCodeFive.h
+++ src/OpCodeFive.h
@@ -24,10 +24,7 @@ public:
     OpCodeFive(
         GetElementAtFunctionType &getElementAt,
         GetIterFromPosPlusOffsetFunctionType &getIterFromPosPlusOffset,
-        const ParameterModeVector &parameterModes)
-        : _getElementAt{getElementAt}, _getIterFromPosPlusOffset{ getIterFromPosPlusOffset },
-            _parameterModes{parameterModes}
-    {}
+        const ParameterModeVector &parameterModes);
 
     ~OpCodeFive() final = default;
 
@@ -35,63 +32,7 @@ public:
     * If the first parameter is non-zero then set the instruction pointer to the value from the second parameter.
     * Otherwise do nothing.
     */
-    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final
-    {
-        IteratorType &iterBegin = std::any_cast<IteratorType &>(nextElementIter);
-        IteratorType &iterEnd = std::any_cast<IteratorType &>(endIter);
-
-        // Check if there are enough numbers to be claimed to complete the operation.
-        if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 2))
-        {
-            return {std::nullopt, {}};
-        }
-
-        // Claim the first parameter based on parameter mode.
-        InputType option;
-        if (_parameterModes[0] == ParameterMode::Immediate)
-        {
-            option = *iterBegin;
-        }
-        else if (_parameterModes[0] == ParameterMode::Position)
-        {
-            option = _getElementAt(iterBegin);
-        }
-        // ParameterMode::Relative
-        else
-        {
-            option = _getElementAt(iterBegin, {});
-        }
-
-        // Jump to the next number no matter what the option value is.
-        iterBegin++;
-
-        // If zero number then do nothing.
-        if (option == 0)
-        {
-            // Skip the number.
-            iterBegin++;
-            return {std::make_optional<Result>(), {iterBegin}};
-        }
-
-        // Move the instruction pointer to the index pointed by the second number.
-        InputType number;
-        if (_parameterModes[1] == ParameterMode::Immediate)
-        {
-            number = *iterBegin;
-        }
-            // ParameterMode::Position
-        else
-        {
-            number = _getElementAt(iterBegin);
-        }
-
-        // Update instruction pointer.
-        iterBegin = _getIterFromPosPlusOffset(number);
-
-        // What we return here it is only useful for error reporting.
-        // Whatever different than std::nullopt is equal to SUCCESS.
-        return {std::make_optional<Result>(), {iterBegin}};
-    }
+    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final;
 
 private:
     GetElementAtFunctionType &_getElementAt;
