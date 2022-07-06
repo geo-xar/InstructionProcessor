c5b32014759ad8b3c06223cb35da819639f20e40
Georgios Charitos <geoxarg@gmail.com>
2022-07-06 22:04:59 +0100

    Moved OpCodeNine implementation to a source file.

--- src/OpCodeNine.h
+++ src/OpCodeNine.h
@@ -23,51 +23,14 @@ public:
     OpCodeNine(
         GetElementAtType& getElementAt,
         UpdateRelativeBaseType& updateRelativeBase,
-        const ParameterModeVector& parameterModes)
-        : _getElementAt{ getElementAt }, _updateRelativeBase{ updateRelativeBase }, _parameterModes{ parameterModes }
-    {}
+        const ParameterModeVector& parameterModes);
 
     ~OpCodeNine() final = default;
 
     /**
     * It adjusts the relative base by the value of its only parameter.
     */
-    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final
-    {
-        IteratorType &iterBegin = std::any_cast<IteratorType &>(nextElementIter);
-        IteratorType &iterEnd = std::any_cast<IteratorType &>(endIter);
-
-        // Check if there are enough numbers to be claimed to complete the operation.
-        // A single number is needed to retrieve the relative base.
-        if (!AreThereEnoughElementsIntoTheCollection(iterBegin, iterEnd, 1))
-        {
-            return {std::nullopt, {}};
-        }
-
-        InputType newRelativeBase{0};
-        if (_parameterModes[0] == ParameterMode::Immediate)
-        {
-            newRelativeBase = *iterBegin;
-        }
-        else if (_parameterModes[0] == ParameterMode::Position)
-        {
-            newRelativeBase = _getElementAt(iterBegin);
-        }
-        // ParameterMode::Relative
-        else
-        {
-            newRelativeBase = _getElementAt(iterBegin, {});
-        }
-
-        _updateRelativeBase(newRelativeBase);
-
-        // Jump to the next number (if any).
-        iterBegin++;
-
-        // What we return here it is only useful for error reporting.
-        // Whatever different than std::nullopt is equal to SUCCESS.
-        return {std::make_optional<Result>(), {iterBegin}};
-    }
+    [[nodiscard]] OpCode::ReturnType Execute(std::any &nextElementIter, std::any &endIter) final;
 
 private:
     GetElementAtType& _getElementAt;
